1fbdbd8d25396b60a4d54b7c157abee1a06e2a43
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-02-29 16:17:25 +0000

    try empty set symbol

--- R/symbols.R
+++ R/symbols.R
@@ -1,1 +1,1 @@
-.virtual_panel_symbol <- ifelse(knitr::is_html_output(), '<i class="fa fa-eye-slash"></i>', '\\O')
+.virtual_panel_symbol <- ifelse(knitr::is_html_output(), '<i class="fa fa-eye-slash"></i>', '$\\emptyset$')
